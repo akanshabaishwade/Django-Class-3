4ef704d1eae42c599b3a254a71957b4ce09f3781
akanshabaishwade <akanshabaishwade@gmail.com>
2024-12-06 08:45:57 +0530

    [Add] Day 3 and 4

---
+++ Day4/venv-create.py
@@ -0,0 +1,10 @@
+# Create venv:
+
+# python -m venv venv_name
+# Activate venv:
+
+# Windows: venv_name\Scripts\activate
+# Linux/Mac: source venv_name/bin/activate
+# Deactivate venv:
+
+# deactivate
