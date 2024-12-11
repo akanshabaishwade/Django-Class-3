87457ea2e14e9f458260f9d4ccdfe85f47b0ed76
akanshabaishwade <akanshabaishwade@gmail.com>
2024-12-11 08:48:57 +0530

    [Add] Day 6

---
+++ project_name/appname/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+# http://127.0.0.1:8000/app/home/
+
+
+urlpatterns = [
+    # path('home/', views.home, name='home'),
+
+    
+]
--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps
-    # 'appname',
+    'appname',
     # libraries
     
 ]
