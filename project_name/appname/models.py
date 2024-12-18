c3882b92dca3ee381a6af05a2c9b0d338a4f20c5
akansha baishwade <akanshabaishwade@gmial.com>
2024-12-18 09:05:56 +0530

    [Add] Day 9

--- project_name/appname/models.py
+++ project_name/appname/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class ModelName(models.Model): #id  (update, delete)
+    field1 = models.CharField(max_length=100)
+    field2 = models.CharField(max_length=50, blank=True, null=True, help_text="enter name of field")
+
+    def __str__(self):
+        return self.field1
+    
+
+
+
+
